719c7474eec7082a17c0c41ba890b68e9e522787
felipecolor9 <felipe.m199@gmail.com>
2024-05-15 22:03:24 -0300

    feat: initial commit

---
+++ src/main/java/br/com/prosperah/api/appcore/AppcoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.prosperah.api.appcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prosperah/api/appcore/AppcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prosperah.api.appcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
