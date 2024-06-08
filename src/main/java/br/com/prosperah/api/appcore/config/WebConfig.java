f898916472c6463d3de084b712a86257be091985
felipecolor9 <felipe.m199@gmail.com>
2024-06-08 18:11:53 -0300

    feat: swagger config/interceptor features

--- src/main/java/br/com/prosperah/api/appcore/config/WebConfig.java
+++ src/main/java/br/com/prosperah/api/appcore/config/WebConfig.java
@@ -9,9 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new ApiHandlerInterceptor());
+        String pathPattern = "/psph/**";
+        String pathPattern2 = "/swagger-ui";
+        registry.addInterceptor(new ApiHandlerInterceptor()).excludePathPatterns(pathPattern2).addPathPatterns(pathPattern);
     }
 
 
